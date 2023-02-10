08bcd6eb8473e282fa12f81ae1ecd73e139a7e5f
Gabriel Henrique <gabrielhsm20@gmail.com>
2023-02-10 00:34:32 -0300

    implementing expense entity

--- src/expense/entities/expense.entity.ts
+++ src/expense/entities/expense.entity.ts
@@ -1,1 +1,9 @@
-export class Expense {}
+export class Expense {
+  id?: number;
+  id_user: number;
+  description: string;
+  value: number;
+  date: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
