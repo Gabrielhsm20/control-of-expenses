304ae2e830106a3019649978afef6ac197c50906
Gabriel Henrique <gabrielhsm20@gmail.com>
2023-02-10 04:16:15 -0300

    exporting new decorator

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserExistsValidator } from './decorators/user-exists-validator.decorator';
+import { UserNotExistsValidator } from './decorators/user-not-exists-validator.decorator';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, UserExistsValidator],
-  exports: [UserService, UserExistsValidator],
+  providers: [UserService, UserExistsValidator, UserNotExistsValidator],
+  exports: [UserService, UserExistsValidator, UserNotExistsValidator],
 })
 export class UserModule {}
